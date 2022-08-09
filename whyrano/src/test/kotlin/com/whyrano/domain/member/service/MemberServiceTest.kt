895f318fbaab23b11a319006b417bcdd3bfba682
shinD <huipulci1@naver.com>
2022-08-09 21:46:51 +0900

    [FEAT] [#1] 회원가입 테스트코드 Mock 대신 testDB를 사용하도록 변경

--- whyrano/src/test/kotlin/com/whyrano/domain/member/service/MemberServiceTest.kt
+++ whyrano/src/test/kotlin/com/whyrano/domain/member/service/MemberServiceTest.kt
@@ -1,27 +1,25 @@
 package com.whyrano.domain.member.service
 
-import com.ninjasquad.springmockk.MockkBean
 import com.whyrano.domain.member.fixture.MemberFixture.EMAIL
 import com.whyrano.domain.member.fixture.MemberFixture.createMemberDto
-import com.whyrano.domain.member.fixture.MemberFixture.member
 import com.whyrano.domain.member.repository.MemberRepository
-import io.mockk.every
-import io.mockk.verify
-import org.junit.jupiter.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
-import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 
 /**
  * Created by ShinD on 2022/08/09.
  */
-@ExtendWith(SpringExtension::class)
+@DataJpaTest
 internal class MemberServiceTest{
 
-    @MockkBean
+    @Autowired
     private lateinit var memberRepository: MemberRepository
+
     private lateinit var memberService: MemberService
 
 
@@ -33,20 +31,19 @@ internal class MemberServiceTest{
 
 
 
-
     @Test
     @DisplayName("회원 가입 성공")
     fun test_signup_success() {
         //given
         val createMemberDto = createMemberDto()
-        every { memberRepository.findByEmail(createMemberDto.email) } returns null
-        every { memberRepository.save(any()) } returns member()
 
         //when
         memberService.signUp(createMemberDto)
 
         //then
-        verify { memberRepository.save(any()) }
+        val findMember = memberRepository.findByEmail(createMemberDto.email)
+        assertThat(findMember?.id).isNotNull
+        assertThat(findMember!!.nickname).isEqualTo(createMemberDto.nickname)
     }
 
     @Test
@@ -54,17 +51,10 @@ internal class MemberServiceTest{
     fun test_signup_fail_by_duplicated_email() {
         //given
         val createMemberDto = createMemberDto(email = EMAIL)
-        every { memberRepository.findByEmail(createMemberDto.email) } returns member()
+        memberService.signUp(createMemberDto)
 
-
-        //when
-        Assertions.assertThrows(IllegalStateException::class.java){memberService.signUp(createMemberDto)}
-
-
-        //then
-        verify (exactly = 1){ memberRepository.findByEmail(any()) }
-        verify (exactly = 0){ memberRepository.save(any()) }
+        //when, then
+        assertThrows(IllegalStateException::class.java) { memberService.signUp(createMemberDto) }
     }
-
 }
 
