fe9519655dc5de7c6b4905eac58cca05e343b7e5
shinD <huipulci1@naver.com>
2022-08-09 19:33:02 +0900

    [FIX] [#1] member의 email에 대한 unique 제약조건 이름 변경
    ### why?
    - @Column을 통해 생성하면 unique 제약조건의 이름이 알아볼 수 없게 생성되어 관리가 어려워질 것 같았음.

--- whyrano/src/main/kotlin/com/whyrano/domain/member/Member.kt
+++ whyrano/src/main/kotlin/com/whyrano/domain/member/Member.kt
@@ -4,12 +4,14 @@ package com.whyrano.domain.member
  * Created by ShinD on 2022/08/09.
  */
 
-import com.whyrano.domain.common.BaseEntity
 import com.whyrano.domain.common.BaseTimeEntity
 import javax.persistence.*
 
 @Entity
-@Table(name = "MEMBER")
+@Table(
+    name = "MEMBER",
+    uniqueConstraints = [UniqueConstraint(columnNames = ["email"], name = "unique_email")]
+    )
 class Member(
 
     @Id @Column(name = "member_id")
@@ -19,7 +21,7 @@ class Member(
     @Column(length = 10, nullable = false)
     var authority: Role,
 
-    @Column(length = 50, unique = true, nullable = false)
+    @Column(name = "email", length = 50, nullable = false)
     var email: String,
 
     @Column(nullable = false)
