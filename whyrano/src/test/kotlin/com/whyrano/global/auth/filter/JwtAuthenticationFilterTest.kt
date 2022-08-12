2c7ba7795dfd8cf04e70731f0ea53b7ea677f68f
shinD <huipulci1@naver.com>
2022-08-12 15:06:27 +0900

    [REFACTOR] [#7] JwtAuthenticationFilterTest 테스트코드 MockKExtension을 사용하도록 수정

--- whyrano/src/test/kotlin/com/whyrano/global/auth/filter/JwtAuthenticationFilterTest.kt
+++ whyrano/src/test/kotlin/com/whyrano/global/auth/filter/JwtAuthenticationFilterTest.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.ninjasquad.springmockk.MockkBean
 import com.whyrano.domain.member.entity.AccessToken
 import com.whyrano.domain.member.entity.RefreshToken
+import com.whyrano.domain.member.entity.Role
 import com.whyrano.domain.member.fixture.MemberFixture.accessToken
 import com.whyrano.domain.member.fixture.MemberFixture.member
 import com.whyrano.domain.member.fixture.MemberFixture.refreshToken
@@ -13,17 +14,18 @@ import com.whyrano.global.auth.jwt.JwtService
 import com.whyrano.global.auth.jwt.JwtService.Companion.ACCESS_TOKEN_HEADER_NAME
 import com.whyrano.global.auth.jwt.JwtService.Companion.ACCESS_TOKEN_HEADER_PREFIX
 import com.whyrano.global.auth.jwt.JwtService.Companion.REFRESH_TOKEN_HEADER_NAME
-import com.whyrano.global.auth.jwt.JwtServiceImpl
 import com.whyrano.global.auth.jwt.TokenDto
-import com.whyrano.global.config.JwtConfig
 import com.whyrano.global.config.SecurityConfig
 import io.mockk.every
+import io.mockk.junit5.MockKExtension
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.context.annotation.Import
+import org.springframework.security.core.userdetails.User
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
@@ -32,28 +34,30 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
  * Created by ShinD on 2022/08/11.
  */
 @WebMvcTest
-@Import(JwtServiceImpl::class, JwtConfig::class, JwtServiceImpl::class, SecurityConfig::class)
+@ExtendWith(MockKExtension::class)
+@Import(SecurityConfig::class)
+@MockkBean(MemberService::class, MemberRepository::class)
 internal class JwtAuthenticationFilterTest {
 
 
-    private val objectMapper = ObjectMapper()
+    companion object {
+        private val objectMapper = ObjectMapper()
+        private val userDetails = User.builder().username("USERNAME").password("PASSWORD").roles(Role.BASIC.name).build()
+    }
+
+
     @Autowired
     private lateinit var mockMvc: MockMvc
 
     @MockkBean
-    private lateinit var memberRepository: MemberRepository
-
-    @MockkBean
-    private lateinit var memberService: MemberService
-
-    @Autowired
     private lateinit var jwtService: JwtService
+
+
 
 
     @Test
     @DisplayName("permitAll 요청 url인 경우 처리 X")
     fun test_permitAll_no_filtering() {
-        //given
         mockMvc
             .perform(get("/login"))
             .andExpect(status().isUnauthorized)
@@ -63,6 +67,8 @@ internal class JwtAuthenticationFilterTest {
     @DisplayName("토큰이 존재하지 않는 경우 403")
     fun test_no_token_403() {
         //given
+        every { jwtService.extractToken(any()) } returns null // AccessToken에 Bearer이 안 붙으면 null을 반환하는 것은 JwtServiceTest에서 확인
+
         mockMvc
             .perform(get("/test"))
             .andExpect(status().isForbidden)
@@ -73,8 +79,10 @@ internal class JwtAuthenticationFilterTest {
     @DisplayName("Access 토큰 하나만 존재하는 경우 403")
     fun test_only_access_token_403() {
         //given
-        every { memberRepository.findByEmail(any()) } returns member()
+        every { jwtService.extractToken(any()) } returns null // AccessToken이 하나만 존재하면 null을 반환하는 것은 JwtServiceTest에서 확인
         val tokenDto = createTokenDto(accessToken())
+
+
         mockMvc
             .perform(get("/test")
                 .header(ACCESS_TOKEN_HEADER_NAME, ACCESS_TOKEN_HEADER_PREFIX + tokenDto.accessToken)
@@ -91,8 +99,10 @@ internal class JwtAuthenticationFilterTest {
     @DisplayName("Refresh 토큰 하나만 존재하는 경우 403")
     fun test_only_refresh_token_403() {
         //given
-        every { memberRepository.findByEmail(any()) } returns member()
+        every { jwtService.extractToken(any()) } returns null // AccessToken에 Bearer이 안 붙으면 null을 반환하는 것은 JwtServiceTest에서 확인
         val tokenDto = createTokenDto(refreshToken = refreshToken())
+
+
         mockMvc
             .perform(get("/test")
                 .header(REFRESH_TOKEN_HEADER_NAME,   tokenDto.refreshToken)
@@ -107,8 +117,10 @@ internal class JwtAuthenticationFilterTest {
     @DisplayName("Access 토큰에 Bearer이 안 붙은 경우 - (AccessToken,RefreshToken 모두 정상 ) - 403")
     fun `Access 토큰에 Bearer이 안 붙은 경우 - (AccessToken,RefreshToken 모두 정상 ) - 403`() {
         //given
-        every { memberRepository.findByEmail(any()) } returns member()
         val tokenDto = createTokenDto(accessToken(), refreshToken())
+        every { jwtService.extractToken(any()) } returns null // AccessToken에 Bearer이 안 붙으면 null을 반환하는 것은 JwtServiceTest에서 확인
+
+
         mockMvc
             .perform(get("/test")
                 .header(ACCESS_TOKEN_HEADER_NAME,  tokenDto.accessToken)
@@ -122,9 +134,13 @@ internal class JwtAuthenticationFilterTest {
     @DisplayName("Access 토큰이 만료된 경우 - (AccessToken,RefreshToken 모두 정상 ) - 200에 토큰 재발급")
     fun `Access 토큰이 만료된 경우 - (AccessToken,RefreshToken 모두 정상 ) - 200에 토큰 재발급`() {
         //given
-        every { memberRepository.findByEmail(any()) } returns member()
-        every { memberRepository.findByAccessTokenAndRefreshToken(any(), any()) } returns member()
         val tokenDto = createTokenDto(accessToken(accessTokenExpirationPeriodDay = -1),  refreshToken())
+        every { jwtService.extractToken(any()) } returns tokenDto
+        every { jwtService.isValidMoreThanMinute(any(), any()) } returns false
+        every { jwtService.isValid(any()) } returns true //RefreshToken은 만료되지 않음
+        every { jwtService.findMemberByTokens(any(), any()) } returns member()
+        every { jwtService.createAccessAndRefreshToken(any()) } returns tokenDto
+
 
         val andReturn = mockMvc
             .perform(
@@ -147,10 +163,12 @@ internal class JwtAuthenticationFilterTest {
     @DisplayName("Access 토큰이 만료된 경우, Refresh 토큰도 만료되었을 때 - 403")
     fun `Access 토큰이 만료된 경우, Refresh 토큰도 만료되었을 때 - 403`() {
         //given
-        every { memberRepository.findByEmail(any()) } returns member()
         val tokenDto = createTokenDto(accessToken(accessTokenExpirationPeriodDay = -1),  refreshToken(refreshTokenExpirationPeriodDay = -1))
+        every { jwtService.extractToken(any()) } returns tokenDto
+        every { jwtService.isValidMoreThanMinute(any(), any()) } returns false
+        every { jwtService.isValid(any()) } returns false // refresh Token의 만료
 
-         mockMvc
+        mockMvc
             .perform(
                 get("/test")
                     .header(ACCESS_TOKEN_HEADER_NAME, ACCESS_TOKEN_HEADER_PREFIX + tokenDto.accessToken)
@@ -169,9 +187,11 @@ internal class JwtAuthenticationFilterTest {
     @DisplayName("Access 토큰이 만료된 경우 - (AccessToken 정상, RefreshToken이 회원의 것과 다를 때 ) - 403")
     fun `Access 토큰이 만료된 경우 - (AccessToken 정상, RefreshToken이 회원의 것과 다를 때 ) - 403`() {
         //given
-        every { memberRepository.findByEmail(any()) } returns member()
-        every { memberRepository.findByAccessTokenAndRefreshToken(any(), any()) } returns null
         val tokenDto = createTokenDto(accessToken(accessTokenExpirationPeriodDay = -1),  refreshToken())
+        every { jwtService.extractToken(any()) } returns tokenDto
+        every { jwtService.isValidMoreThanMinute(any(), any()) } returns false
+        every { jwtService.isValid(any()) } returns true
+        every { jwtService.findMemberByTokens(any(), any()) } returns null
 
         mockMvc
             .perform(
@@ -187,9 +207,10 @@ internal class JwtAuthenticationFilterTest {
     @DisplayName("Access 토큰이 만료된 경우 - (AccessToken, RefreshToken 모두 회원의 것과 다를 때 ) - 403")
     fun `Access 토큰이 만료된 경우 - (AccessToken, RefreshToken 모두 회원의 것과 다를 때 ) - 403`() {
         //given
-        every { memberRepository.findByEmail(any()) } returns member()
-        every { memberRepository.findByAccessTokenAndRefreshToken(any(), any()) } returns null
         val tokenDto = createTokenDto(accessToken(accessTokenExpirationPeriodDay = -1),  refreshToken())
+        every { jwtService.extractToken(any()) } returns tokenDto
+        every { jwtService.isValidMoreThanMinute(any(), any()) }  returns false
+        every { jwtService.isValid(any()) }  returns false
 
         mockMvc
             .perform(
@@ -207,11 +228,12 @@ internal class JwtAuthenticationFilterTest {
     @DisplayName("Access 토큰이 만료되지 않은 경우 - Userdetail이 정상인 경우 -> 인증 성공 (404)")
     fun `Access 토큰이 만료되지 않은 경우 - Userdetail이 정상인 경우 - 인증 성공 (404)`() {
         //given
-        every { memberRepository.findByEmail(any()) } returns member()
-        every { memberRepository.findByAccessTokenAndRefreshToken(any(), any()) } returns member()
         val tokenDto = createTokenDto(accessToken(),  refreshToken())
+        every { jwtService.extractToken(any()) } returns tokenDto
+        every { jwtService.isValidMoreThanMinute(any(), any()) } returns true
+        every { jwtService.extractUserDetail(any()) } returns userDetails
 
-             mockMvc
+        mockMvc
             .perform(
                 get("/test")
                     .header(ACCESS_TOKEN_HEADER_NAME, ACCESS_TOKEN_HEADER_PREFIX + tokenDto.accessToken)
@@ -220,6 +242,4 @@ internal class JwtAuthenticationFilterTest {
             .andExpect(status().isNotFound)
 
     }
-
-
 }
