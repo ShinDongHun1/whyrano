e85d0f7b90a1bfe60f7bd534e91b7bea6e9e3845
shinD <huipulci1@naver.com>
2022-08-13 01:51:33 +0900

    [FEAT] [#6] 회원 수정 기능 구현

--- whyrano/src/main/kotlin/com/whyrano/domain/member/controller/MemberController.kt
+++ whyrano/src/main/kotlin/com/whyrano/domain/member/controller/MemberController.kt
@@ -1,13 +1,16 @@
 package com.whyrano.domain.member.controller
 
 import com.whyrano.domain.member.controller.dto.request.CreateMemberRequest
+import com.whyrano.domain.member.controller.dto.request.UpdateMemberRequest
 import com.whyrano.domain.member.service.MemberService
-import mu.KotlinLogging
+import com.whyrano.global.auth.userdetails.AuthMember
+import com.whyrano.global.web.argumentresolver.Auth
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder.*
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder.fromCurrentContextPath
 import javax.validation.Valid
 
 /**
@@ -17,9 +20,6 @@ import javax.validation.Valid
 class MemberController(
     private val memberService: MemberService
 ) {
-
-    private val log = KotlinLogging.logger {  }
-
 
     /**
      * 회원 가입 요청
@@ -36,6 +36,18 @@ class MemberController(
         return ResponseEntity.created(url).build()
     }
 
+    /**
+     * 회원 수정 -> 자기 자신만 수정할 수 있음
+     *
+     */
+    @PutMapping("/member")
+    fun update(
+        @Auth authMember: AuthMember,
+        @RequestBody umr: UpdateMemberRequest) : ResponseEntity<Unit> {
+
+        memberService.update(authMember.id, umr.toServiceDto())
+        return ResponseEntity.ok().build()
+    }
 
 
 
