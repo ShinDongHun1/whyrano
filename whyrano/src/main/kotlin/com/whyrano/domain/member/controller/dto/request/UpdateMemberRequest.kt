edb5c15334ae5fab8863611773b587725602d865
shinD <huipulci1@naver.com>
2022-08-13 01:36:04 +0900

    [FEAT] [#6] AuthMemberArgumentResolver 정의, @Auth를 통한 AuthMember 바인딩

---
+++ whyrano/src/main/kotlin/com/whyrano/domain/member/controller/dto/request/UpdateMemberRequest.kt
@@ -0,0 +1,16 @@
+package com.whyrano.domain.member.controller.dto.request
+
+import com.whyrano.domain.member.service.dto.UpdateMemberDto
+
+/**
+ * Created by ShinD on 2022/08/13.
+ */
+data class UpdateMemberRequest(
+    var password: String? = null,
+    var nickname: String? = null,
+    var profileImagePath: String? = null,
+) {
+    fun toServiceDto(): UpdateMemberDto =
+        UpdateMemberDto(password, nickname, profileImagePath)
+
+}
