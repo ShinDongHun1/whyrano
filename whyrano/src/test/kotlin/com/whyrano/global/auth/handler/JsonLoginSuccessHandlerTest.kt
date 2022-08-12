6b029ccdeb875be6ca18b8535dcd55daa8965ee4
shinD <huipulci1@naver.com>
2022-08-12 14:17:37 +0900

    [TEST] [#7] JsonLoginSuccessHandlerTest 테스트코드 MockKExtension을 사용하도록 수정

--- whyrano/src/test/kotlin/com/whyrano/global/auth/handler/JsonLoginSuccessHandlerTest.kt
+++ whyrano/src/test/kotlin/com/whyrano/global/auth/handler/JsonLoginSuccessHandlerTest.kt
@@ -2,46 +2,47 @@ package com.whyrano.global.auth.handler
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.ninjasquad.springmockk.MockkBean
-import com.whyrano.domain.member.fixture.MemberFixture
+import com.whyrano.domain.member.fixture.MemberFixture.createMemberDto
 import com.whyrano.domain.member.service.MemberService
 import com.whyrano.global.auth.jwt.JwtService
 import com.whyrano.global.auth.jwt.TokenDto
 import com.whyrano.global.config.SecurityConfig
 import com.whyrano.global.config.SecurityConfig.Companion.LOGIN_URL
 import io.mockk.every
+import io.mockk.junit5.MockKExtension
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.context.annotation.Import
 import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.security.core.userdetails.User
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 /**
  * Created by ShinD on 2022/08/10.
  */
 @WebMvcTest
+@ExtendWith(MockKExtension::class)
 @Import(SecurityConfig::class)
-@ActiveProfiles("local")
 internal class JsonLoginSuccessHandlerTest {
     companion object {
         private val objectMapper = ObjectMapper()
+        private var passwordEncoder: PasswordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder()
     }
+
 
     @Autowired
     private lateinit var mockMvc: MockMvc
 
     @MockkBean
     private lateinit var memberService: MemberService
-
-    @Autowired
-    private lateinit var passwordEncoder: PasswordEncoder
 
     @MockkBean
     private lateinit var jwtService: JwtService
@@ -51,7 +52,7 @@ internal class JsonLoginSuccessHandlerTest {
     @DisplayName("로그인 성공시 JWT 발급, 상태코드 200")
     fun test_login_success_publish_jwt() {
         //given
-        val memberDto = MemberFixture.createMemberDto()
+        val memberDto = createMemberDto()
         val member = memberDto.toEntity(passwordEncoder)
         every { memberService.loadUserByUsername(member.email) } returns User.builder().username(member.email).password(member.password).roles(member.role.name).build()
         every { jwtService.createAccessAndRefreshToken(any()) } returns TokenDto("Access", "ref")
@@ -62,13 +63,13 @@ internal class JsonLoginSuccessHandlerTest {
         //when
         val result = mockMvc
             .perform(
-                MockMvcRequestBuilders.post(LOGIN_URL)
+                post(LOGIN_URL)
                     .contentType(APPLICATION_JSON)
                     .content(
                         objectMapper.writeValueAsString(hashMap)
                     )
             )
-            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(status().isOk)
             .andReturn()
 
 
