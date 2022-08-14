c778be4db488b42b3af974ceee8abe9e8f3769ff
shinD <huipulci1@naver.com>
2022-08-14 20:21:04 +0900

    [FIX] [#9] MemberAuditorAware 삭제
    - 작성자가 없는 경우 영속성 컨텍스트 flush 시 오류가 발생함 (테스트 시 불편함)

--- whyrano/src/main/kotlin/com/whyrano/domain/post/entity/Post.kt
+++ whyrano/src/main/kotlin/com/whyrano/domain/post/entity/Post.kt
@@ -4,10 +4,12 @@ package com.whyrano.domain.post.entity
  * Created by ShinD on 2022/08/11.
  */
 
-import com.whyrano.domain.common.BaseEntity
 import com.whyrano.domain.common.BaseTimeEntity
+import com.whyrano.domain.member.entity.Member
+import org.springframework.data.annotation.CreatedBy
 import javax.persistence.*
 import javax.persistence.EnumType.STRING
+import javax.persistence.FetchType.LAZY
 import javax.persistence.GenerationType.IDENTITY
 
 @Entity
@@ -28,12 +30,25 @@ class Post(
     @Column(name = "content", nullable = false)
     var content: String, // 내용
 
-    var answerCount: Int, // 답변 수
+    var answerCount: Int = 0, // 답변 수
 
-    var viewCount: Int, // 조회수
+    var viewCount: Int = 0, // 조회수
 
-    var likeCount: Int, // 좋아요 개수
+    var likeCount: Int = 0, // 좋아요 개수
 
 
-) : BaseEntity() { // 작성자는 BaseEntity에 포함
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "writer", nullable = false)
+    var writer: Member? = null // 작성자
+
+) : BaseTimeEntity() {
+
+
+    /**
+     * 작성자 설정
+     */
+    fun confirmWriter(writer: Member) {
+        this.writer = writer
+    }
 }
