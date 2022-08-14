9d18db4b5654be959cef71b7eb9aab6ca9fc866a
shinD <huipulci1@naver.com>
2022-08-14 17:41:05 +0900

    [FEAT] [#9] Post Entity 생성

--- whyrano/src/main/kotlin/com/whyrano/domain/post/entity/Post.kt
+++ whyrano/src/main/kotlin/com/whyrano/domain/post/entity/Post.kt
@@ -7,12 +7,33 @@ package com.whyrano.domain.post.entity
 import com.whyrano.domain.common.BaseEntity
 import com.whyrano.domain.common.BaseTimeEntity
 import javax.persistence.*
+import javax.persistence.EnumType.STRING
+import javax.persistence.GenerationType.IDENTITY
 
 @Entity
 @Table(name = "POST")
 class Post(
     @Id @Column(name = "post_id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = IDENTITY)
     val id: Long? = null,
-) : BaseEntity() {
+
+    @Enumerated(STRING)
+    @Column(name = "type", nullable = false)
+    val type: Type, // 공지 | 질문
+
+    @Column(name = "title", nullable = false)
+    var title: String, // 제목
+
+    @Lob
+    @Column(name = "content", nullable = false)
+    var content: String, // 내용
+
+    var answerCount: Int, // 답변 수
+
+    var viewCount: Int, // 조회수
+
+    var likeCount: Int, // 좋아요 개수
+
+
+) : BaseEntity() { // 작성자는 BaseEntity에 포함
 }
---
+++ whyrano/src/main/kotlin/com/whyrano/domain/post/entity/Type.kt
@@ -0,0 +1,13 @@
+package com.whyrano.domain.post.entity
+
+/**
+ * Created by ShinD on 2022/08/14.
+ */
+enum class Type {
+
+    QUESTION, // 질문
+
+    NOTICE, // 공지
+
+    ;
+}
