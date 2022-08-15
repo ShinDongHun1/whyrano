4115fe622fe17c4d704a49cc22095770fc74245c
shinD <huipulci1@naver.com>
2022-08-16 02:45:29 +0900

    [FEAT] [#9] Querydsl을 사용하여 게시물 동적 검색 & 정렬 기능 구현

--- whyrano/src/main/kotlin/com/whyrano/domain/post/search/PostSearchCond.kt
+++ whyrano/src/main/kotlin/com/whyrano/domain/post/search/PostSearchCond.kt
@@ -1,32 +1,12 @@
 package com.whyrano.domain.post.search
 
-import com.whyrano.domain.post.entity.Type
-import org.springframework.data.domain.Sort.Direction
-import org.springframework.data.domain.Sort.Direction.DESC
+import com.whyrano.domain.post.entity.PostType
 
 /**
  * Created by ShinD on 2022/08/15.
  */
 data class PostSearchCond(
-
-    // 내용 일치로 검색
-    val content: String? = null,
-    val title: String? = null,
-
-    val type: Type? = null,
-
-    // 조회수가 높은거부터
-    val viewCount: Direction = DESC,
-
-    // 좋아요 수가 높은거부터
-    val likeCount: Direction = DESC,
-
-    // 답변 수가 높은거부터
-    val answerCount: Direction = DESC,
-
-    // 최신에 생성된 것 부터
-    val createdDate: Direction = DESC,
-
-    // 댓글 개수가 많은 것 부터
-    val commentCount: Direction = DESC,
+    val content: String? = null,    // 검색할 내용
+    val title: String? = null,      // 검색할 제목
+    val postType: PostType? = null,         // 검색할 타입
 )
