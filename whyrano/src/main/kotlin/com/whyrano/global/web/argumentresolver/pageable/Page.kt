ba5fba4d811d3c874b493c65188a43e0cd8742cd
shinD <huipulci1@naver.com>
2022-08-16 21:30:26 +0900

    [FIX] [#9] Page 어노테이션에 메타 어노테이션 추가

--- whyrano/src/main/kotlin/com/whyrano/global/web/argumentresolver/pageable/Page.kt
+++ whyrano/src/main/kotlin/com/whyrano/global/web/argumentresolver/pageable/Page.kt
@@ -1,6 +1,11 @@
 package com.whyrano.global.web.argumentresolver.pageable
 
+import kotlin.annotation.AnnotationRetention.RUNTIME
+import kotlin.annotation.AnnotationTarget.VALUE_PARAMETER
+
 /**
  * Created by ShinD on 2022/08/16.
  */
+@Target(VALUE_PARAMETER)
+@Retention(RUNTIME)
 annotation class Page
