90e4798fdd8e7feb03b03ad354d7820791f8638e
shinD <huipulci1@naver.com>
2022-08-16 20:49:41 +0900

    [FEAT] [#9] 페이지를 1부터 시작할 수 있도록 Pageable 객체를 반환해주는 Start1PageableArgumentResolver 구현

--- whyrano/src/main/kotlin/com/whyrano/global/config/WebConfig.kt
+++ whyrano/src/main/kotlin/com/whyrano/global/config/WebConfig.kt
@@ -1,6 +1,7 @@
 package com.whyrano.global.config
 
-import com.whyrano.global.web.argumentresolver.AuthMemberArgumentResolver
+import com.whyrano.global.web.argumentresolver.auth.AuthMemberArgumentResolver
+import com.whyrano.global.web.argumentresolver.pageable.Start1PageableArgumentResolver
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
@@ -8,17 +9,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 /**
  * Created by ShinD on 2022/08/13.
  */
+
+//@EnableWebMvc
 @Configuration
 class WebConfig(
 
     private val authMemberArgumentResolver: AuthMemberArgumentResolver,
 
-) : WebMvcConfigurer { //TODO WebMvcConfigurationSupport 와의 차이가 뭘까..?
+    private val start1PageableArgumentResolver: Start1PageableArgumentResolver,
+
+    ) : WebMvcConfigurer { // WebMvcConfigurationSupport 와의 차이가 뭘까..? -> http://honeymon.io/tech/2018/03/13/spring-boot-mvc-controller.html
 
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
 
         super.addArgumentResolvers(resolvers)
 
         resolvers.add(authMemberArgumentResolver)
+        resolvers.add(start1PageableArgumentResolver)
     }
 }
---
+++ whyrano/src/main/kotlin/com/whyrano/global/web/argumentresolver/pageable/Page.kt
@@ -0,0 +1,6 @@
+package com.whyrano.global.web.argumentresolver.pageable
+
+/**
+ * Created by ShinD on 2022/08/16.
+ */
+annotation class Page
