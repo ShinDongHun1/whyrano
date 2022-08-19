c1dba70a2a4684c4d30dfc591fdfe834830d11d0
shinD <huipulci1@naver.com>
2022-08-19 20:25:47 +0900

    [TEST] [#17] TaggedPostRepository 사용되는 메서드에 대한 테스트코드 작성

--- whyrano/src/test/kotlin/com/whyrano/domain/taggedpost/repository/TaggedPostRepositoryTest.kt
+++ whyrano/src/test/kotlin/com/whyrano/domain/taggedpost/repository/TaggedPostRepositoryTest.kt
@@ -21,10 +21,18 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 @MockkBean(JPAQueryFactory::class)
 class TaggedPostRepositoryTest {
 
-    @Autowired private lateinit var taggedPostRepository: TaggedPostRepository
-    @Autowired private lateinit var postRepository: PostRepository
-    @Autowired private lateinit var tagRepository: TagRepository
-    @Autowired private lateinit var memberRepository: MemberRepository
+    @Autowired
+    private lateinit var taggedPostRepository: TaggedPostRepository
+    @Autowired
+    private lateinit var postRepository: PostRepository
+    @Autowired
+    private lateinit var tagRepository: TagRepository
+    @Autowired
+    private lateinit var memberRepository: MemberRepository
+
+
+
+
 
     @Test
     fun `saveAll 동작- 가장 빠름`() {
@@ -33,7 +41,7 @@ class TaggedPostRepositoryTest {
         val member = MemberFixture.member(id = null)
         memberRepository.save(member)
 
-        val post = PostFixture.post(id= null)
+        val post = PostFixture.post(id = null)
         post.confirmWriter(member)
 
         val tags = TagFixture.newTags(size = 3)
@@ -47,11 +55,68 @@ class TaggedPostRepositoryTest {
         val saveAll = taggedPostRepository.saveAll(taggedPosts)
 
         //then
-        taggedPosts.forEach{ assertThat(it.id).isNotNull() }
-        saveAll.forEach{ assertThat(it.id).isNotNull() }
-
-
+        taggedPosts.forEach { assertThat(it.id).isNotNull() }
+        saveAll.forEach { assertThat(it.id).isNotNull() }
     }
 
 
+
+
+
+    @Test
+    fun `findByPost 동작`() {
+
+        //given
+        val member = MemberFixture.member(id = null)
+        memberRepository.save(member)
+
+        val post = PostFixture.post(id = null)
+        post.confirmWriter(member)
+
+        val tags = TagFixture.newTags(size = 3)
+
+        postRepository.save(post)
+        tagRepository.saveAll(tags)
+        val taggedPosts = TaggedPostFixture.newTaggedPosts(post = post, tags = tags)
+        val saveAll = taggedPostRepository.saveAll(taggedPosts)
+
+        //when
+        val savedTaggedPost = taggedPostRepository.findByPost(post)
+
+        //then
+        assertThat(savedTaggedPost.size).isEqualTo(tags.size)
+        savedTaggedPost.forEach { assertThat(it.post.id).isEqualTo(post.id) }
+    }
+
+
+
+
+
+    @Test
+    fun `deleteAllInBatch 동작`() {
+
+        //given
+        val member = MemberFixture.member(id = null)
+        memberRepository.save(member)
+
+        val post = PostFixture.post(id = null)
+        post.confirmWriter(member)
+
+        val tags = TagFixture.newTags(size = 5)
+
+        postRepository.save(post)
+        tagRepository.saveAll(tags)
+        val taggedPosts = TaggedPostFixture.newTaggedPosts(post = post, tags = tags)
+        val saveAll = taggedPostRepository.saveAllAndFlush(taggedPosts)
+
+        val savedTaggedPost = taggedPostRepository.findByPost(post)
+
+
+        //when
+        taggedPostRepository.deleteAllInBatch(savedTaggedPost)
+
+
+        //then
+        assertThat(taggedPostRepository.findByPost(post).size).isEqualTo(0)
+    }
 }
