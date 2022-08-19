403853992f91eec857950cf79f909219a19e7364
shinD <huipulci1@naver.com>
2022-08-19 19:36:12 +0900

    [FEAT] [#17] Tag DTO 추가

---
+++ whyrano/src/main/kotlin/com/whyrano/domain/tag/dto/TagDto.kt
@@ -0,0 +1,17 @@
+package com.whyrano.domain.tag.dto
+
+import com.whyrano.domain.tag.entity.Tag
+
+/**
+ * Created by ShinD on 2022/08/19.
+ */
+data class TagDto(
+
+    val id: Long? = null, // null이 아닌 경우 이미 존재하는 태그
+
+    val name: String,
+
+) {
+    fun toEntity() =
+        Tag(id = id, name = name)
+}
